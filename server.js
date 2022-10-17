40e07a4147daa50af7a44fff648a084f9e4f65e5
cmash93 <cmash93@gmail.com>
2022-10-17 15:31:57 -0500

    set up server page

--- server.js
+++ server.js
@@ -7,9 +7,28 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
 
 // GET Route for index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 // GET Route for notes.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+// GET Route for index.html using the wildcard
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 // Listening notification within console
+app.listen(PORT, () => {
+    console.log(`App listening at http//localhost:${PORT}`)
+})
