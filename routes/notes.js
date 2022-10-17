2e44fddfc6fb0fa17a416a88c310d3598df0965f
cmash93 <cmash93@gmail.com>
2022-10-17 15:37:00 -0500

    setting up middleware/routers

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,13 @@
 const notes = require('express').Router();
 
 // GET Route for retrieving notes
+notes.get('/', (res, req) => {
+    
+});
 
 // POST Route for new note
+notes.post('/', (res, req) => {
+    
+})
 
 module.exports = notes;
