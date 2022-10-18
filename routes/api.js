be36995abe4ec3dbced39e56fcc113ca95a5b49f
cmash93 <cmash93@gmail.com>
2022-10-18 02:28:01 -0500

    get/post are actually happeninggit add .! it's still not fully functional though

--- routes/api.js
+++ routes/api.js
@@ -2,35 +2,46 @@ const api = require('express').Router();
 const fs = require('fs');
 const util = require('util');
 const uuid = require('uuid');
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+const readFromFile = util.promisify(fs.readFile);
+let db = require('../db/db.json');
 
 
 // GET Route for retrieving notes
 api.get('/notes', (req, res) => {
-    readFileSync('db/db.json', 'utf8')
+    readFromFile('./db/db.json')
     .then((data) => {
-        res.json(data)
+        res.json(JSON.parse(data))
     })
 });
 
 // POST Route for new note
 api.post('/notes', (req, res) => {
-    readFileAsync('db/db.json', 'utf8')
-    .then((data) => {
+    console.log(req.body);
 
-        let newNote = req.body;
-        newNote.id = uuid.v4();
-    
-        data.push(newNote);
-        writeFileAsync('./db/db.json', JSON.stringify(data))
-        .then((err) => {
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid.v4()   
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
                 console.log(err)
-            } else { console.log('Note has been added successfully') }
-        });
-        res.send(data)
-    });
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(newNote);
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData), (err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log('Note successfully created!')
+                    }
+                })
+            }
+        })
+    }
 });
 
 // DELETE note
